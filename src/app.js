30b0d1e46369bbf90444512d6e9de0449f46577f
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2021-04-22 19:56:47 -0300

    Habilitado o CORS para todos os domínios

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ import routeModules from './routes';
 const app = express();
 const { NOT_FOUND } = messages;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(httpLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
